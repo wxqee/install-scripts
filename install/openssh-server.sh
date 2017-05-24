f5652f8eabd514ebd48317e2b14d1a0501d7bb15
Robin Wang <robin.wang@otms.com>
2017-05-24 21:33:54 +0800

    openssh-server good

--- install/openssh-server.sh
+++ install/openssh-server.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-echo TODO: complete this script for install ssh-server.
+(lsb_release -a 2>/dev/null | grep 'Ubuntu 16' >/dev/null) || {
+  echo "This script is for Ubuntu 16"
+  exit 1
+}
+
+set -x
+sudo apt install -y openssh-server
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.original
+sudo chmod a-w /etc/ssh/sshd_config.original
+set +x
+
+sudo sed -e '/^PubkeyAuthentication/d' -e '$a\
+PubkeyAuthentication yes
+' -i /etc/ssh/sshd_config
+
+set -x
+cat /etc/ssh/sshd_config | grep PubkeyAuthentication
+set +x
 
